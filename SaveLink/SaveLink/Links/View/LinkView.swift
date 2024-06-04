0587f2700b65bb9eb8297ee703e9c926ea8743ab
Rubén <ruben.sr.apple@gmail.com>
2024-06-04 13:28:38 +0200

    add box to create a link in view

--- SaveLink/SaveLink/Links/View/LinkView.swift
+++ SaveLink/SaveLink/Links/View/LinkView.swift
@@ -10,9 +10,36 @@ import SwiftUI
 struct LinkView: View {
     
     @ObservedObject var linkViewModel: LinkViewModel
+    @State var text: String = ""
     
     var body: some View {
         VStack {
+            
+            TextEditor(text: $text)
+                .frame(height: 100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.green, lineWidth: 2)
+                )
+                .padding(.horizontal, 12)
+                .cornerRadius(3)
+            
+            Button(action: {
+                linkViewModel.createNewLink(fromURL: text)
+            }, label: {
+                Label("Crear Link", systemImage: "link")
+            })
+            .tint(.green)
+            .controlSize(.regular)
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.capsule)
+            
+            if linkViewModel.messageError != nil {
+                Text(linkViewModel.messageError!)
+                    .bold()
+                    .foregroundStyle(.red)
+            }
+            
             List {
                 
                 ForEach(linkViewModel.links) { link in
