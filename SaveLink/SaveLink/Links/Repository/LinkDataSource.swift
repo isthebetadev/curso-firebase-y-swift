e7d7bb7644719f5358adaea82035a707fef7da58
Rubén <ruben.sr.apple@gmail.com>
2024-06-04 12:11:17 +0200

    add linkDataSource GetAllLinks

--- SaveLink/SaveLink/Links/Repository/LinkDataSource.swift
+++ SaveLink/SaveLink/Links/Repository/LinkDataSource.swift
@@ -14,5 +14,31 @@ struct LinkModel: Decodable, Identifiable {
     let url: String
     let title: String
     let isFavorite: Bool
-    let isViewed: Bool 
+    let isViewed: Bool
+}
+
+final class LinkDataSource {
+    
+    private let database = Firestore.firestore()
+    private let collection = "links"
+
+    func getAllLinks(completionBlock: @escaping (Result<[LinkModel], Error>) -> Void) {
+        database.collection(collection)
+            .addSnapshotListener { query, error in
+                if let error = error {
+                    print("Error getting all links \(error.localizedDescription)")
+                    completionBlock(.failure(error))
+                    return
+                }
+                guard let documents = query?.documents.compactMap({ $0 }) else {
+                    completionBlock(.success([]))
+                    return
+                }
+                let links = documents.map { try? $0.data(as: LinkModel.self) }
+                                     .compactMap { $0 }
+                completionBlock(.success(links))
+            }
+                            
+    }
+    
 }
--- SaveLink/SaveLink/Links/Repository/LinkRepository.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LinkRepository.swift
-//  SaveLink
-//
-//  Created by Ruben on 4/6/24.
-//
-
-import Foundation
