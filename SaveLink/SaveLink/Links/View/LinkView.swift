285d50018eba46d5eb24ecaf095c136d571b47f1
Rubén <ruben.sr.apple@gmail.com>
2024-06-04 12:45:05 +0200

    Refactor view

--- SaveLink/SaveLink/Links/View/LinkView.swift
+++ SaveLink/SaveLink/Links/View/LinkView.swift
@@ -14,29 +14,38 @@ struct LinkView: View {
     var body: some View {
         VStack {
             List {
+                
                 ForEach(linkViewModel.links) { link in
+                    
                     Text(link.title)
                         .bold()
                         .lineLimit(4)
                         .padding(.bottom, 8)
+                    
                     Text(link.url)
                         .foregroundStyle(.gray)
                         .font(.caption)
+                    
                     HStack {
+                        
                         Spacer()
+                        
                         if link.isViewed {
                             Image(systemName: "checkmark.circle.fill")
                                 .resizable()
                                 .foregroundStyle(.blue)
-                                .frame(width: 10, height: 10)
+                                .frame(width: 10, height: 12)
                         }
+                        
                         if link.isFavorite {
                             Image(systemName: "star.fill")
                                 .resizable()
                                 .foregroundStyle(.yellow)
-                                .frame(width: 10, height: 10)
+                                .frame(width: 10, height: 12)
                         }
+                        
                     }
+                    
                 }
             }
             .task {
